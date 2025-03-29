9a0182222a987ef03b172d5d190e93e5dc34e4fb
Matanzror <mattan862@gmail.com>
2025-03-29 19:57:56 +0300

    changes in MatanZror.java

--- MatanZror.java
+++ MatanZror.java
@@ -1,7 +1,7 @@
 public class MatanZror{
 	public static void main(String [] args){
 		for (int i=1; i<=10; i++){
-			System.out.println("Matan Zror");
+			System.out.println("Matan Zror is here");
 		}
 	}
 }
